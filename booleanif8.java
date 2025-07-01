621450147936cced9a626b832cec318a2488c696
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:23:55 +0530

    Add files via upload

---
+++ booleanif8.java
@@ -0,0 +1,15 @@
+class booleanif8{
+    public static void main(String args[])
+    {
+        boolean rain=false;
+        if(rain)
+        {
+            System.out.println("take an umbrella");
+        }
+        else{
+            System.out.println("enjoy the rain");
+        }
+
+        
+    }
+}
