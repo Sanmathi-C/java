95b783d74d6f62bdd40e4b85794052fb921ea8de
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:22:10 +0530

    Add files via upload

---
+++ codecc6.java
@@ -0,0 +1,61 @@
+// q1
+import java.lang.System;
+import java.util.Scanner;
+class codecc6{
+    public static void main(String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        String name = san.nextLine();
+        int age = san.nextInt();
+        System.out.println("My name is"+name);
+        System.out.println("My age is"+age);
+    }
+}
+// q2
+import java.lang.System;
+import java.util.Scanner;
+class codecc6{
+    public static void main (String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        String name = san.nextLine();
+        int age = san.nextInt();
+        san.nextLine();
+        String address = san.nextLine();
+        System.out.println("my name is" +name);
+        System.out.println("my age is" +age);
+        System.out.println("my address is" +address);
+
+    }
+}
+// q3
+import java.lang.System;
+import java.util.Scanner;
+class codecc{
+    public static void main(String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        int a = san.nextInt();
+        int b = san.nextInt();
+        int c = san.nextInt();
+        int d = a*b*c;
+        int e = a+b+c;
+        System.out.println(d/e);
+    }
+}
+// q4
+import java.lang.System;
+import java.util.Scanner;
+class codecc{
+    public static void main(String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        String name = san.nextLine();
+        double score = san.nextInt();
+        san.nextLine();
+        String department = san.nextLine();
+        System.out.println("my name is" + name);
+        System.out.println("my score is"+ score/10);
+        System.out.println("my department is"+department);
+    }
+}
