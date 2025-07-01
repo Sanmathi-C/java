da0ac0da7f3c3008f83e1be461b36984fcd23c56
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:23:24 +0530

    Add files via upload

---
+++ comparisonif8.java
@@ -0,0 +1,14 @@
+class comparisonif8{
+    public static void main(String args[])
+    {
+        int num1=56;
+        int num2=98;
+        if(num1>num2)
+        {
+            System.out.println("num1 is greater");
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+    }
+}
