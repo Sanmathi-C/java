8d1d062fdbe6f0be75e2c4d1a495e7a2b1e013bd
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:20:38 +0530

    Add files via upload

---
+++ threeno4.java
@@ -0,0 +1,10 @@
+class threeno4{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=10;
+        int c=8;
+        int d=a+b+c;
+        System.out.println("d");
+    }
+}
