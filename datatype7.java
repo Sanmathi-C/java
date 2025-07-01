b9d323e43a0d857553b19d271deaf3423882c2aa
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:22:43 +0530

    Add files via upload

---
+++ datatype7.java
@@ -0,0 +1,19 @@
+class datatype7{
+    public static void main (String args[])
+    {
+        byte a = 34;
+        short b=987;
+        int c=9567;
+        long d=68573;
+        float e=3.96f;
+        double f=67.765444444;
+        String g="hello world";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g.charAt(5));
+    }
+}
