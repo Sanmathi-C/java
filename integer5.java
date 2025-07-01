a67813d29c9c74ce3676b79cc7f3ad24687806fc
sanmathi <sanmathic05@gmail.com>
2025-07-01 18:21:14 +0530

    Add files via upload

---
+++ integer5.java
@@ -0,0 +1,23 @@
+// /in int
+import java.lang.System;
+import java.util.Scanner;
+class integer5{
+    public static void main(String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        int a = san.nextInt();
+        System.out.println(a);
+    }
+
+}
+// string
+import java.lang.System;
+import java.util.Scanner;
+class integer5{
+    public static void main(String args[])
+    {
+        Scanner san = new Scanner(System.in);
+        String a = san.nextLine();
+        System.out.println(a);
+    }
+}
