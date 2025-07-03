a73cef19eb3ad7c9d6563f85edcf0f3d29569621
sanmathi <sanmathic05@gmail.com>
2025-07-03 18:30:43 +0530

    Add files via upload

---
+++ stringcon9.java
@@ -0,0 +1,54 @@
+class stringcon9{
+    public static void main (String args[])
+    {
+        String f1=new String("apple");
+        String f2=new String("apple");
+        System.out.println(f1.equals(f2));
+
+    }
+}
+//q1
+class stringcon9{
+public static void main (String args[])
+{
+    String a="one";
+    String b="one";
+    String c=a;
+    System.out.println(a==c);
+    
+
+}
+}
+//q2
+class stringcon9{
+    public static void main (String args[])
+    {
+        String RCB="win";
+        if(RCB.equals("no"))
+        {
+            System.out.println("esale cup namdhae");
+        }
+        else{
+            System.out.println("no cup");
+        }
+    }
+}
+//q3
+import java.util.Scanner;
+class stringcon9{
+    public static void main(String args[])
+    {
+        Scanner san=new Scanner(System.in);
+        String a= san.nextLine();
+        String meghana=san.nextLine;
+        if(meghana.equals("dead"))
+        {
+            System.out.println("Surya meets Ramya");
+
+        }
+        else{
+            System.out.println("Surya weds meghana");
+        }
+
+    }
+}
