7b446e2775b2ec393033ceabdc5e4fa318cc283e
sanmathi <sanmathic05@gmail.com>
2025-07-10 11:16:34 +0530

    Add files via upload

---
+++ nestedif11.java
@@ -0,0 +1,20 @@
+class nestif11{
+    public static void main(String args[])
+    {
+        boolean kfc=true;
+        boolean chicken=false;
+        boolean pepsi=true;
+        if(kfc)
+        {
+            System.out.println("enter kfc");
+            if(chicken)
+            {
+                System.out.println("eat chicken");
+                if(pepsi)
+                {
+                    System.out.println("drink pepsi");
+                }
+            }
+        }
+    }
+}
