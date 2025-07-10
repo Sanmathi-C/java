f5f1f218ad62e3b57ed19e1888e16792e1c6bfe0
sanmathi <sanmathic05@gmail.com>
2025-07-10 11:15:48 +0530

    Add files via upload

---
+++ logical10.java
@@ -0,0 +1,32 @@
+////and
+class logical10{
+    public static void main(String args[])
+
+{
+    boolean hungry=true;
+    boolean ice=false;
+    if(hungry && ice)
+    {
+        System.out.println("eat");
+    }
+    else{
+        System.out.println("no eat");
+    }
+}
+}
+////or
+class logical10{
+    public static void main(String args[])
+
+{
+    boolean hungry=true;
+    boolean ice=false;
+    if(hungry || ice)
+    {
+        System.out.println("eat");
+    }
+    else{
+        System.out.println("no eat");
+    }
+}
+}
